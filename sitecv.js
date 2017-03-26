a729d68dbed0c042877519a2adcf4aa6250ed10a
emga <emelinegatty@gmail.com>
2017-03-27 00:14:52 +0200

    first

---
+++ sitecv.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+  $('.js-scrollTo').on('click', function() { // Au clic sur un élément
+			var page = $(this).attr('href'); // Page cible
+			var speed = 1000; // Durée de l'animation (en ms)
+			$('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+			return false;
+		});
+
+    $(function(){
+      setInterval(function(){
+         $(".slideshow ul").animate({marginLeft:-350},800,function(){
+            $(this).css({marginLeft:0}).find("li:last").after($(this).find("li:first"));
+         })
+      }, 2000);
+   });
+
+    $(function(){
+      setInterval(function(){
+          $(".slide ul").animate({marginLeft:-250},800,function(){
+            $(this).css({marginLeft:0}).find("li:last").after($(this).find("li:first"));
+          })
+        }, 2000);
+      });
+
+
+});
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
